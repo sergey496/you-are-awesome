dfe88a84e83cd7b58cfabffaa3b8d86976959499
Sergey Zylevich <sergey496@yandex.ru>
2018-03-12 11:54:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 // DO WHATEVER YOU WANT HERE
 
 const createEnumerableProperty = (propertyName) => {this.propertyName=propertyName;};
-const createNotEnumerableProperty = () => {	};
+const createNotEnumerableProperty = (property) => {Object.defineProperty(Object.prototype, property, {enumerable: false, value: 'value'});return property;};
 const createProtoMagicObject = () => {let results = new Function();results.prototype = Function.prototype;return results};
 const incrementor = () => { incrementor.value = incrementor.value || 0; function val() {	incrementor.value++; 	return val;	} val.toString = function () {return incrementor.value;}; return val();};
-const asyncIncrementor = () => {};
+var asyncCounter = 0;
+const asyncIncrementor = () => {asyncCounter++;Function.prototype.valueOf = function() {return asyncCounter;};return asyncIncrementor;};
 const createIncrementer = () => {next (); {return {value}}};
 
 // return same argument not earlier than in one second, and not later, than in two
 const returnBackInSecond = (param) => {return new Promise(resolve => {setTimeout(() => {return resolve(param);}, 1000);})};
-const getDeepPropertiesCount = () => {};
+const getDeepPropertiesCount = (obj) => {var counter = 0;count(obj);function count(property) {for (var n in property) {counter++;if (Object.keys(property[n]).length > 0) {count(property[n]);};};};return counter;};
 const createSerializedObject = () => {return new String();};
 const toBuffer = () => {};
 const sortByProto = (arr) => {return arr.sort((left, right) => left.__proto__ - right.__proto__)};
